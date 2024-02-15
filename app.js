7fb20e6488a884b77fa05a2eaff729b008c1e8b3
Noam Sokolove <62836489+JSninjawizard@users.noreply.github.com>
2024-02-15 14:11:24 +0200

    s

--- app.js
+++ app.js
@@ -1,64 +1,74 @@
-function nameVal (name) {
-    const nameLength = name.length
-    const isSpace = name.includes(" ")
+function nameVal(name) {
+  const nameLength = name.length;
+  const isSpace = name.includes(" ");
 
-    const nameToArr = name.split("")
-    const UnScores = nameToArr.filter((el) => el.includes("_"))
+  const nameToArr = name.split("");
+  const UnScores = nameToArr.filter((el) => el.includes("_"));
 
-    if (nameLength <= 3 || isSpace || UnScores.length >=2) {
-        console.log('wrong!!');
-    } else {
-        console.log(name);
-    }
-
+  if (nameLength <= 3 || isSpace || UnScores.length >= 2) {
+    console.log("wrong!!");
+  } else {
+    console.log(name);
+  }
 }
 
-nameVal("skasdfa")
+nameVal("skasdfa");
 
 // ---
 
-const input = document.querySelector("input")
-const btn = document.querySelector("button")
-const validator = document.querySelector("h5")
+const input = document.querySelector("input");
+const btn = document.querySelector("button");
+const validator = document.querySelector("h5");
 
+validator.textContent = "";
 
-validator.textContent = ""
-
-btn.addEventListener("click", function(){
-    const inputName = input.value
-    validateName(inputName)
-})
+btn.addEventListener("click", function () {
+  const inputName = input.value;
+  validateName(inputName);
+});
 
 validateName = (inputName) => {
-    const iLength = inputName.length
-    const iSpace = inputName
-    const iUnderScore = inputName
+  const iLength = inputName.length;
+  const iSpace = inputName;
+  const iUnderScore = inputName;
+  const specialChars = inputName;
 
-    lengthCheck(iLength)
-    spaceCheck(iSpace)
-    underscoreCheck(iUnderScore)
-}
+  lengthCheck(iLength);
+  spaceCheck(iSpace);
+  underscoreCheck(iUnderScore);
+
+  const err1 = lengthCheck(iLength);
+  console.log(err1);
+  const err2 = spaceCheck(iSpace);
+  const err3 = underscoreCheck(iUnderScore);
+
+  if (err1 === undefined && err2 === undefined && err3 === undefined) {
+    validator.textContent = "Correct!";
+  } else if (err1 !== undefined || err2 !== undefined || err3 !== undefined) {
+    validator.textContent = `${err1}, ${err2}, ${err3}`
+  } else if (err1 === undefined && err2 !== undefined && err3 !== undefined) {
+    validator.textContent = `${err2}, ${err3}`
+  }
+};
 
 lengthCheck = (iLength) => {
-    if (iLength <= 3) {
-        return validator.textContent = 'Name is too short'
-    } 
-}
+    console.log(iLength);
+  if (iLength <= 3) {
+    return validator.textContent = "Name is too short"
+  }
+};
 
 spaceCheck = (iSpace) => {
-    if (iSpace.includes(" ")) {
-        return validator.textContent = 'Name has space character'
-    }
-}
+  if (iSpace.includes(" ")) {
+    return validator.textContent = "Name has space character"
+  }
+};
 
 underscoreCheck = (iUnderScore) => {
-    const toArr = iUnderScore.split("")
-    const count = toArr.filter((el) => el.includes("_"))
+  const toArr = iUnderScore.split("");
+  const count = toArr.filter((el) => el.includes("_"));
 
-    if (count.length >= 2) {
-        return validator.textContent = "Name has more than 2 underscore characters"
-    }
-}
-
-
-
+  if (count.length >= 2) {
+    return validator.textContent = "Name has more than 2 underscore characters"
+  }
+};
