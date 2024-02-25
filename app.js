6f682ea4ecd4d9381beefff9f315ae0f10a1ef9f
Noam Sokolove <62836489+JSninjawizard@users.noreply.github.com>
2024-02-25 20:01:47 +0200

    good

--- app.js
+++ app.js
@@ -23,7 +23,24 @@ for (const el of arrayOfAnswers) {
     const listItem = document.createElement("li")
     listItem.textContent = el
     ordered.appendChild(listItem)
-    
+    if (listItem.textContent.includes("at least 3")) {
+        listItem.classList.add('correct')
+    }
+    if (listItem.textContent.includes("too short")) {
+        listItem.classList.add('wrong')
+    } 
+    if (listItem.textContent.includes("String has no spaces")) {
+        listItem.classList.add('correct')
+    }
+    if (listItem.textContent.includes("Number of spaces:")) {
+        listItem.classList.add('wrong')
+    } 
+    if (listItem.textContent.includes("Number of underscores:")) {
+        listItem.classList.add('wrong')
+    } 
+    if (listItem.textContent.includes("String has allowed number of underscores")) {
+        listItem.classList.add('correct')
+    } 
 }
 
 // const orderedList = document.createElement("ol")
@@ -50,7 +67,7 @@ strLength = (str) => {
 strSpace = (spaces) => {
     const spaceCount = spaces.split(" ").length - 1
     if (spaceCount != 0) {
-        return `String has ${spaceCount} spaces`
+        return `Number of spaces: ${spaceCount}`
     } else {
         return `String has no spaces`
     }
@@ -59,7 +76,7 @@ strSpace = (spaces) => {
 strUnderscore = (underscore) => {
     const underscoreCount = underscore.split("_").length -1
     if (underscoreCount > 1) {
-        return `String has ${underscoreCount} underscores`
+        return `Number of underscores: ${underscoreCount}`
     } else {
         return `String has allowed number of underscores`
     }
