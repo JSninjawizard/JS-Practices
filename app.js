82bb8b5a419951e9a6921e9f35da0293aecc732b
Noam Sokolove <62836489+JSninjawizard@users.noreply.github.com>
2024-02-25 09:17:40 +0200

    f1

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const input = document.querySelector("input");
 const btn = document.querySelector("button");
 const validator = document.querySelector("h5");
+const ordered = document.querySelector("ul");
 
 validator.textContent = "";
 let arrayOfAnswers = []
@@ -13,10 +14,29 @@ const inputLength = strLength(inputName)
 const hasSpace = strSpace(inputName)
 const hasUnderscore = strUnderscore(inputName)
 
-const validate = val(inputLength, hasSpace, hasUnderscore)
+val(inputLength, hasSpace, hasUnderscore)
+
 arrayOfAnswers = []
 arrayOfAnswers.push(inputLength, hasSpace, hasUnderscore)
-console.log(arrayOfAnswers);
+
+for (const el of arrayOfAnswers) {
+    const listItem = document.createElement("li")
+    listItem.textContent = el
+    ordered.appendChild(listItem)
+    
+}
+
+// const orderedList = document.createElement("ol")
+// const listItem = document.createElement("li")
+// for (const el of arrayOfAnswers) {
+//     console.log(el);
+//     listItem.textContent = el
+//     orderedList.appendChild(listItem)
+//     validator.appendChild(orderedList)  
+
+// }
+    
+
 });
 
 strLength = (str) => {
@@ -46,19 +66,6 @@ strUnderscore = (underscore) => {
 }
 
 val = (a,b,c) => {
-    // if (a === true) {
-    //     console.log('str > 3');
-    // } if (a === false) {
-    //     console.log('str < 3');
-    // } if (b === true) {
-    //     console.log('str has space');
-    // } if (b === false) {
-    //     console.log("str has no space");
-    // } if (c === true) {
-    //     console.log('str has more that 1 underscore');
-    // } if (c === false) {
-    //     console.log('undersocres count ok');
-    // }
         // const orderedList = document.createElement("ol")
         // const listItem = document.createElement("li")
         // listItem.textContent = `${a}, ${b}, ${c}`
